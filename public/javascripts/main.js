d3b440bf1dba22b36eaed1a1c0f2427ec363d85c
Macbook Air <macbookair@David-Ramirez.local>
2017-01-06 23:07:59 -0800

    update the jquery call

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,18 @@
-
 $(function(){
+  var source = $("#search-results").html();
+  var dataTemplate = Handlebars.compile(source);
+  $results = $('#results')
+
   $('#search').on('keyup', function(e){
-    if(e.keyCode === 13) {
-     var parameters = { search: $(this).val() };
-     console.log(parameters)
-       $.get( '/searching',parameters, function(data) {
-         console.log(data)
-       $('#results').html(data);
-     });
-    };
- });
+  	if(e.keyCode === 13) {
+			var parameters = { search: $(this).val() };
+  		$.get('/searching', parameters, function(data){
+    		if (data instanceof Array) {
+          $results.html(dataTemplate({resultsArray:data}));
+        } else {
+          $results.html(data);
+        };
+  		});
+  	};
+  });
 });
